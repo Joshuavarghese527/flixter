e50fda1c000c7c1abf74a8df2af06237bef528e2
Joshua Varghese <joshuavarghese527@gmail.com>
2016-05-10 17:37:18 +0000

    Video embeding and Styling the Student Course Show Page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,6 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap-sprockets
 //= require jquery_ujs
-
-
 //= require_tree .
-// Loads all Bootstrap javascripts
-//= require bootstrap
