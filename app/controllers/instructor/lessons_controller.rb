d094053c3d44f43978851e3e8770b866cff57169
Joshua Varghese <joshuavarghese527@gmail.com>
2016-05-18 18:44:45 +0000

    made changes to sections

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -17,7 +17,7 @@ class Instructor::LessonsController < ApplicationController
   end
 
   def lesson_params
-    params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
+    params.require(:lesson).permit(:title, :subtitle, :video, :row_order)
   end
  
   private 
